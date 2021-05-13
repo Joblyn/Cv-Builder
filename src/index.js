92a1461f57e61da110de9a01b2374aac1123993a
Joblyn <oaikhenahjob@gmail.com>
2021-05-13 04:46:00 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import * as serviceWorker from './serviceWorker';
 import "normalize.css";
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { GlobalStyles } from './global-styles';
-import * as serviceWorker from './serviceWorker';
 import { firebase } from './lib/firebase.prod';
 import { FirebaseContext } from './context/firebase';
 import store from './store';
